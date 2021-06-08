25e0f6580584e5b9cbb3f8b5b62441fa7b9f36cb
artursem <artur.semeniuk@gmail.com>
2021-06-08 20:18:31 +0200

    add form validation

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('hello world!');
+const inputFirstName = document.querySelector('#first-name');
+const inputLastName = document.querySelector('#last-name');
+const inputEmail = document.querySelector('#email');
+const inputPassword = document.querySelector('#password');
+const errorMessage = document.querySelectorAll('.error');
+const btn = document.querySelector('button');
+
+const validateInput = (inputName, number) => {
+    if (inputName.value !== '') {
+        errorMessage[number].classList.add('display-none');
+        inputName.classList.remove('input-error');
+    } else {
+        errorMessage[number].classList.remove('display-none');
+        inputName.classList.add('input-error');
+    }
+}
+
+const validateEmail = (number) => {
+    const regex = new RegExp(/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi);
+    const test = regex.test(inputEmail.value);
+
+    if (test) {
+        errorMessage[number].classList.add('display-none');
+        inputEmail.classList.remove('input-error');
+    } else {
+        errorMessage[number].classList.remove('display-none');
+        inputEmail.classList.add('input-error');
+    }
+    
+}
+
+btn.addEventListener('click', e => {
+    e.preventDefault();
+
+    validateInput(inputFirstName, 0);
+    validateInput(inputLastName, 1);
+    validateEmail(2);
+    validateInput(inputPassword, 3);
+});
